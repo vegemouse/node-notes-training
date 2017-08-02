d89fe1b459f650393a0a61e28f459aab178b5d11
Maxwell Cady <maxwell.josef@gmail.com>
2017-08-01 21:53:06 -0700

    save notes

--- notes.js
+++ notes.js
@@ -1,6 +1,50 @@
 console.log('Starting notes.js');
 
-module.exports.addNote = () => {
-  console.log('addNote');
-  return 'New note';
+const fs = require('fs');
+
+var fetchNotes = () => {
+  try {
+    var notesString = fs.readFileSync('notes-data.json');
+    return JSON.parse(notesString);
+  } catch (e) {
+    return [];
+  }
+};
+
+var saveNotes = (notes) => {
+
+};
+
+var addNote = (title, body) => {
+  var notes = fetchNotes();
+  var note = {
+    title,
+    body
+  };
+
+  var duplicateNotes = notes.filter((note) => note.title === title);
+
+  if (duplicateNotes.length === 0) {
+    notes.push(note);
+    fs.writeFileSync('notes-data.json', JSON.stringify(notes));
+  }
+};
+
+var getAll = () => {
+  console.log('Getting all notes');
+};
+
+var getNote = title => {
+  console.log("Reading " + title);
+}
+
+var removeNote = title => {
+  console.log("Removing " + title);
+}
+
+module.exports = {
+  addNote,
+  getAll,
+  getNote,
+  removeNote
 };
